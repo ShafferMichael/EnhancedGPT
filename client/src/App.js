7968ed065076ba7812daa80aed31e50d86b5a924
Michael Shaffer <michaelshaffer45@gmail.com>
2022-12-26 16:38:25 -0500

    edit

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
-import "./normal.css";
+import "./normalize.css";
 
 function App() {
   return (
     <div className="App">
-      <aside className="sidemenu">
-        <h1>Aside</h1>
+      <aside className="side-menu">
+        <div className="side-menu-button">New Chat</div>
       </aside>
       <section className="chatbox"></section>
     </div>
