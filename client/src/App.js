c8e53368033e1b0f058e22be64890c0cfb1ee074
Michael Shaffer <michaelshaffer45@gmail.com>
2022-12-26 18:07:13 -0500

    .

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
 import "./App.css";
 import "./normalize.css";
 
+/**
+ * TODO: add awesomefont font to search bar <span>+</span>
+ */
+
 function App() {
   return (
     <div className="App">
       <aside className="side-menu">
-        <div className="side-menu-button">New Chat</div>
+        <div className="side-menu-button">
+          <span>+</span>
+          New Chat
+        </div>
       </aside>
       <section className="chatbox"></section>
     </div>
