2714d2826ddb586e7f21bc8dfe9f1151a5357ddb
Michael Shaffer <michaelshaffer45@gmail.com>
2022-12-26 15:47:25 -0500

    Added normalize.css git and updated client UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "./normal.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <aside className="sidemenu">
+        <h1>Aside</h1>
+      </aside>
+      <section className="chatbox"></section>
     </div>
   );
 }
