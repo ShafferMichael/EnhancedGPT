fd7a0e09ab37f5b65138dc731d6efd7be108bc32
Michael Shaffer <michaelshaffer45@gmail.com>
2022-12-28 16:51:06 -0500

    starting configuring frontend & backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
-import "./App.css";
 import "./normalize.css";
+import "./App.css";
+import { useState } from "react";
 
 /**
  * TODO: add awesomefont font to search bar <span>+</span>
  */
 
 function App() {
+  // add state for input and chat log
+  const [input, setInput] = useState("");
+  const [chatLog, setChatLog] = useState([]);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setChatLog([...chatLog, { user: "me", message: `${input}` }]);
+    setInput("");
+
+    console.log("submit");
+  }
+
   return (
     <div className="App">
       <aside className="side-menu">
@@ -16,12 +29,8 @@ function App() {
       </aside>
       <section className="chat-box">
         <div className="chat-log">
-          <div className="chat-message">
-            <div className="chat-message-center">
-              <div className="avatar"></div>
-              <div className="message">USER TEXT</div>
-            </div>
-          </div>
+          <ChatMessage message={{ user: "me", message: input }} />
+
           <div className="chat-message chatgpt">
             <div className="chat-message-center">
               <div className="avatar chatgpt">
@@ -44,12 +53,31 @@ function App() {
             </div>
           </div>
         </div>
+
         <div className="chat-input-holder">
-          <textarea className="chat-input-textarea"></textarea>
+          <form onSubmit={handleSubmit}>
+            <input
+              rows="1"
+              value={input}
+              onChange={(e) => setInput(e.target.value)} //diff
+              className="chat-input-textarea"
+            ></input>
+          </form>
         </div>
       </section>
     </div>
   );
 }
+
+const ChatMessage = ({ message }) => {
+  return (
+    <div className={`chat-message ${message.user === "gpt" && "chatgpt"}`}>
+      <div className="chat-message-center">
+        <div className={`avatar ${message.user === "gpt" && "chatgpt"}`}></div>
+        <div className="message">{message.message}</div>
+      </div>
+    </div>
+  );
+};
 
 export default App;
--- index.js
+++ index.js
@@ -1,22 +1,40 @@
-// require the OpenAI API client library
+// Import the OpenAI API client library and the express library
 const { Configuration, OpenAIApi } = require("openai");
+const express = require("express");
 
+// Set up a configuration object with the organization ID and API key for the OpenAI API
 const configuration = new Configuration({
   organization: "org-fdEElitDGbfqLgz6ZLYDyBAH",
   apiKey: "",
 });
 
-// create the OpenAI API instance
+// Create an OpenAI API instance using the configuration object
 const openai = new OpenAIApi(configuration);
 
-async function callApi() {
-  // Create a completion request node.js
+// Create an express app instance
+const app = express();
+
+// Specify the port for the express server to listen on
+const port = 3080;
+
+// Define a route for the root path that listens for POST requests
+app.post("/", async (req, res) => {
+  // When a request is received, create a text completion request using the OpenAI API
   const response = await openai.createCompletion({
     model: "text-davinci-003",
     prompt: "Say this is a test",
     max_tokens: 7,
     temperature: 0,
   });
+  // Log the text of the first completion choice to the console
   console.log(response.data.choices[0].text);
-}
-callApi();
+  // Send the response data back to the client in JSON format
+  res.json({
+    data: response.data,
+  });
+});
+
+// Start the express server and log a message to the console when it is ready to accept requests
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
